3424bfd185e91e44dc78590dd1b7ca796843eba4
JungHyunjun <103713434+ark1210@users.noreply.github.com>
2023-08-10 07:04:14 +0900

    Update hw4_server.c

--- hw4_server.c
+++ hw4_server.c
@@ -7,39 +7,34 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <ctype.h>
+
+#define MAX_LINES 256
 #define BUF_SIZE 100
 #define MAX_CLNT 256
-#define TRIE_SIZE 27
-
-typedef struct TrieNode
+typedef struct packet
 {
-    struct TrieNode *children[TRIE_SIZE];
-    int isEndOfWord;
-} TrieNode;
+    int count;
+    char buf[BUF_SIZE];
+    int line_num;
+} pkt;
 
-// Global variables
-TrieNode *root;
-int clnt_cnt = 0;
-int clnt_socks[MAX_CLNT];
-pthread_mutex_t mutx;
-
-// Function declarations
 void *handle_clnt(void *arg);
 void send_msg(char *msg, int len);
 void error_handling(char *msg);
-TrieNode *createNode(void);
-void insert(TrieNode *root, const char *word);
-int search(TrieNode *root, const char *word);
-void printTrie(TrieNode *root, char *buffer, int level);
-int searchRecursive(TrieNode *node, const char *word, int index);
-int searchPartial(TrieNode *node);
-// Main function
+void printPackets();
+void searchPacket(const char *searchTerm, int sock);
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+pthread_mutex_t mutx;
+pkt packets[MAX_LINES];
+
 int main(int argc, char *argv[])
 {
     int serv_sock, clnt_sock;
     struct sockaddr_in serv_adr, clnt_adr;
     int clnt_adr_sz;
     pthread_t t_id;
+
     if (argc != 2)
     {
         printf("Usage : %s <port>\n", argv[0]);
@@ -59,11 +54,15 @@ int main(int argc, char *argv[])
     if (listen(serv_sock, 5) == -1)
         error_handling("listen() error");
 
-    // Initialize the Trie
-    root = createNode();
-
     FILE *file = fopen("data.txt", "r");
+    // if (!file)
+    // {
+    //     error_handling("Failed to open data.txt");
+    //     return;
+    // }
+
     char line[BUF_SIZE];
+    int line_num = 0;
     while (fgets(line, sizeof(line), file) != NULL)
     {
         line[strcspn(line, "\n")] = 0; // Remove newline character
@@ -72,25 +71,31 @@ int main(int argc, char *argv[])
         char *last_space = strrchr(line, ' ');
         if (last_space && isdigit(*(last_space + 1)))
         {
-            printf("Number: %s\n", last_space + 1); // Print the number
+            // Extract the number
+            packets[line_num].count = atoi(last_space + 1);
 
             // Null-terminate to separate string and number
             *last_space = '\0';
-            printf("String: %s\n", line);
-            insert(root, line);
+            strncpy(packets[line_num].buf, line, BUF_SIZE - 1);
+            packets[line_num].buf[BUF_SIZE - 1] = '\0';
         }
         else
         {
-            printf("String: %s\n", line);
-            insert(root, line);
+            // No number found, save the entire line
+            strncpy(packets[line_num].buf, line, BUF_SIZE - 1);
+            packets[line_num].buf[BUF_SIZE - 1] = '\0';
+            packets[line_num].count = 0; // Or any other default value
         }
+        packets[line_num].line_num = line_num;
+        line_num++;
     }
     fclose(file);
+    printPackets();
 
-    char buffer[BUF_SIZE];
-    printTrie(root, buffer, 0);
+    char userSearchTerm[BUF_SIZE];
 
-    printf("\nsearch : %d\n", search(root, "w"));
+    // 데이터 로딩 후 패킷 검색
+    // searchPacket(userSearchTerm);
 
     while (1)
     {
@@ -102,7 +107,6 @@ int main(int argc, char *argv[])
         pthread_mutex_unlock(&mutx);
 
         pthread_create(&t_id, NULL, handle_clnt, (void *)&clnt_sock);
-
         pthread_detach(t_id);
         printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
     }
@@ -110,183 +114,34 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void printTrie(TrieNode *root, char *buffer, int level)
-{
-    if (root->isEndOfWord)
-    {
-        buffer[level] = '\0'; // Null terminate the word
-        printf("Stored word: %s\n", buffer);
-    }
-
-    for (int i = 0; i < TRIE_SIZE; i++)
-    {
-        if (root->children[i])
-        {
-            if (i == TRIE_SIZE - 1) // 스페이스 문자를 인식하고 처리
-            {
-                buffer[level] = ' ';
-            }
-            else
-            {
-                buffer[level] = i + 'a'; // Convert the index back to a character
-            }
-            printTrie(root->children[i], buffer, level + 1);
-        }
-    }
-}
-
-// Trie related functions
-TrieNode *createNode(void)
-{
-    TrieNode *node = (TrieNode *)malloc(sizeof(TrieNode));
-    node->isEndOfWord = 0;
-    for (int i = 0; i < TRIE_SIZE; i++)
-    {
-        node->children[i] = NULL;
-    }
-    return node;
-}
-void insert(TrieNode *root, const char *word)
-{
-    TrieNode *cur = root;
-    while (*word)
-    {
-        char c = tolower(*word); // 문자를 소문자로 변환
-
-        int index;
-        if (c == ' ') // 스페이스 문자를 인식하고 처리
-        {
-            index = TRIE_SIZE - 1; // 예를 들어 스페이스를 트라이의 마지막 인덱스로 처리
-        }
-        else if (c < 'a' || c > 'z') // 알파벳 문자가 아닌 경우 건너뛰기
-        {
-            word++;
-            continue;
-        }
-        else
-        {
-            index = c - 'a';
-        }
-
-        if (!cur->children[index])
-        {
-            cur->children[index] = createNode();
-        }
-        cur = cur->children[index];
-        word++;
-    }
-    cur->isEndOfWord = 1;
-}
-
-int search(TrieNode *root, const char *word)
-{
-    TrieNode *cur = root;
-    while (*word)
-    {
-        char c = tolower(*word); // 문자를 소문자로 변환
-
-        int index;
-        if (c == ' ') // 스페이스 문자를 인식하고 처리
-        {
-            index = TRIE_SIZE - 1; // 스페이스를 트라이의 마지막 인덱스로 처리
-        }
-        else if (c < 'a' || c > 'z') // 알파벳 문자가 아닌 경우 건너뛰기
-        {
-            word++;
-            continue;
-        }
-        else
-        {
-            index = c - 'a';
-        }
-
-        if (!cur->children[index])
-            return searchPartial(root); // 전체 문자열이 없으면 부분 문자열 검색
-
-        cur = cur->children[index];
-        word++;
-    }
-
-    return cur->isEndOfWord ? 1 : searchPartial(root); // 전체 문자열이 있으면 1, 없으면 부분 문자열 검색
-}
-
-int searchPartial(TrieNode *node)
-{
-    if (node->isEndOfWord)
-        return 1; // 부분 문자열 발견
-    for (int i = 0; i < TRIE_SIZE; i++)
-    {
-        if (node->children[i] && searchPartial(node->children[i]))
-            return 1;
-    }
-    return 0;
-}
-
-// int search(TrieNode *root, const char *word)
-// {
-//     TrieNode *cur = root;
-//     while (*word)
-//     {
-//         char c = tolower(*word); // 문자를 소문자로 변환
-
-//         int index;
-//         if (c == ' ') // 스페이스 문자를 인식하고 처리
-//         {
-//             index = TRIE_SIZE - 1; // 스페이스를 트라이의 마지막 인덱스로 처리
-//             printf("Space character encountered\n");
-//         }
-//         else if (c < 'a' || c > 'z') // 알파벳 문자가 아닌 경우 건너뛰기
-//         {
-//             // printf("Non-alphabet character encountered: %c\n", c);
-//             word++;
-//             continue;
-//         }
-//         else
-//         {
-//             index = c - 'a';
-//         }
-
-//         // printf("Current character: %c, Index: %d\n", c, index);
-
-//         if (!cur->children[index])
-//         {
-//             // printf("Child node not found at index %d, exiting search\n", index);
-//             return 0;
-//         }
-
-//         cur = cur->children[index];
-//         word++;
-//     }
-
-//     // printf("Search completed. End of word flag: %d\n", cur->isEndOfWord);
-//     return cur->isEndOfWord;
-// }
-
-// Other functions are the same as before, with modification to handle_clnt to perform search
 void *handle_clnt(void *arg)
 {
     int clnt_sock = *((int *)arg);
-    int str_len = 0;
-    char msg[BUF_SIZE];
+    int str_len = 0, i;
 
-    while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
+    char userSearchTerm[BUF_SIZE];
+
+    while ((str_len = read(clnt_sock, userSearchTerm, sizeof(userSearchTerm))) != 0)
     {
-        msg[str_len] = '\0'; // 문자열 종료
-        if (search(root, msg))
-        {
-            // 클라이언트에게만 메시지를 전송
-            write(clnt_sock, "Result found!", 14);
-        }
-        else
-        {
-            // 클라이언트에게만 메시지를 전송
-            write(clnt_sock, "No result found!", 17);
-        }
+        // printf("strlen  %d", str_len);
+        userSearchTerm[str_len] = '\0'; // 문자열 종료
+        searchPacket(userSearchTerm, clnt_sock);
     }
 
-    fputs(msg, stderr);
-    fputc('\n', stderr);
-    exit(1);
+    pthread_mutex_lock(&mutx);
+    for (i = 0; i < clnt_cnt; i++) // remove disconnected client
+    {
+        if (clnt_sock == clnt_socks[i])
+        {
+            while (i++ < clnt_cnt - 1)
+                clnt_socks[i] = clnt_socks[i + 1];
+            break;
+        }
+    }
+    clnt_cnt--;
+    pthread_mutex_unlock(&mutx);
+    close(clnt_sock);
+    return NULL;
 }
 void send_msg(char *msg, int len) // send to all
 {
@@ -301,4 +156,69 @@ void error_handling(char *msg)
     fputs(msg, stderr);
     fputc('\n', stderr);
     exit(1);
+}
+
+void printPackets()
+{
+    for (int i = 0; i < MAX_LINES; i++)
+    {
+        if (packets[i].buf[0] == '\0')
+            break; // 문자열이 비어 있으면 루프 종료
+        printf("Line %d\n", packets[i].line_num);
+        printf("Buffer: %s\n", packets[i].buf);
+        printf("Count: %d\n", packets[i].count);
+        printf("------------------------\n");
+    }
+}
+
+void searchPacket(const char *searchTerm, int sock)
+{
+    int found = 0;
+    char result[MAX_LINES * (BUF_SIZE + 20)]; // Enough space for results
+    result[0] = '\0';                         // Initialize result string
+
+    // Storing lines that match the search term
+    pkt foundPackets[MAX_LINES];
+    int foundCount = 0;
+    for (int i = 0; i < MAX_LINES; i++)
+    {
+        if (strstr(packets[i].buf, searchTerm))
+        {
+            foundPackets[foundCount++] = packets[i];
+            found = 1;
+        }
+    }
+
+    // Sorting by count in descending order
+    for (int i = 0; i < foundCount - 1; i++)
+    {
+        for (int j = 0; j < foundCount - i - 1; j++)
+        {
+            if (foundPackets[j].count < foundPackets[j + 1].count)
+            {
+                pkt temp = foundPackets[j];
+                foundPackets[j] = foundPackets[j + 1];
+                foundPackets[j + 1] = temp;
+            }
+        }
+    }
+
+    // Adding sorted results to the result string
+    for (int i = 0; i < foundCount; i++)
+    {
+        // strcat(result, "FOUND in Line ");
+        char line_num_str[10];
+        // sprintf(line_num_str, "%d: ", foundPackets[i].line_num);
+        strcat(result, line_num_str);
+        strcat(result, foundPackets[i].buf);
+        strcat(result, "\n");
+    }
+
+    if (!found)
+    {
+        strcpy(result, "NOT FOUND\n");
+    }
+
+    // Send the result to the client
+    write(sock, result, strlen(result));
 }
