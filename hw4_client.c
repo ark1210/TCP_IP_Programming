0c0b1d3862ab4ce8e4a90a90ae169a1ba7ae74c2
JungHyunjun <103713434+ark1210@users.noreply.github.com>
2023-08-11 20:33:34 +0900

    Update hw4_client.c

--- hw4_client.c
+++ hw4_client.c
@@ -5,6 +5,11 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <termios.h>
+#include <unistd.h>
+#include <termios.h>
+#include <unistd.h>
+#include <ctype.h>
 
 #define MAX_LINES 256	
 #define BUF_SIZE 100
@@ -15,7 +20,8 @@ void * recv_msg(void * arg);
 void error_handling(char * msg);
 void printPackets();
 void searchPacket(const char *searchTerm);	
-
+void printWithHighlight(const char *text, const char *searchTerm);
+int getch(); // getch 함수 선언
 typedef struct packet
 {
     int count; //4
@@ -26,10 +32,13 @@ char name[NAME_SIZE]="[DEFAULT]";
 char msg[BUF_SIZE];
 char temp[PAKETSIZE];
 pkt packets[MAX_LINES];
+pthread_mutex_t lock;
 
+char userSearchTerm[BUF_SIZE] = {0}; // 초기화
 
 int main(int argc, char *argv[])
 {
+	//char userSearchTerm[BUF_SIZE]={0};
 	int sock;
 	struct sockaddr_in serv_addr;
 	pthread_t snd_thread, rcv_thread;
@@ -38,6 +47,8 @@ int main(int argc, char *argv[])
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	 }
+	 // 뮤텍스 초기화
+    pthread_mutex_init(&lock, NULL);
 	
 	//sprintf(name, "[%s]", argv[3]);
 	sock=socket(PF_INET, SOCK_STREAM, 0);
@@ -50,50 +61,148 @@ int main(int argc, char *argv[])
 	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
 		error_handling("connect() error");
 	
+	
 	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
 	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
 	pthread_join(snd_thread, &thread_return);
 	pthread_join(rcv_thread, &thread_return);
+	    // 뮤텍스 제거
+    pthread_mutex_destroy(&lock);
 	close(sock);  
 	return 0;
 }
 	
-void * send_msg(void * arg)   // send thread main
-{
-	int sock=*((int*)arg);
-	//char name_msg[NAME_SIZE+BUF_SIZE];
 
-	char userSearchTerm[BUF_SIZE];
-    
+// void * send_msg(void * arg)
+// {
+// 	int sock = *((int *)arg);
+// 	char userSearchTerm[BUF_SIZE];
+// 	int idx = 0;
 
-	// while(1) 
-	// {
-
-		printf("Search Word: ");
-		fgets(userSearchTerm, sizeof(userSearchTerm), stdin);
-		userSearchTerm[strcspn(userSearchTerm, "\n")] = 0; // Removing newline character
-
-		if(!strcmp(msg,"q\n")||!strcmp(msg,"Q\n")) 
-		{
-			close(sock);
-			exit(0);
-		}
-
-		write(sock, userSearchTerm, strlen(userSearchTerm));
-	//}
-	return NULL;
-}
+// 	printf("Search Word: ");
 	
-void * recv_msg(void * arg)   // read thread main
+// 	while (1) 
+// 	{
+// 		int ch = getch(); // 문자 하나를 받아옴
+		
+// 		if (ch == '\n' || ch == EOF) break; // 엔터 키 누르면 종료
+
+// 		if (ch == 127) // 백스페이스 키 처리
+// 		{
+// 			if (idx > 0) // 이미 입력된 문자가 있을 때만
+// 			{
+// 				putchar('\b'); // 커서를 하나 뒤로 이동
+// 				putchar(' ');  // 현재 위치에 공백 출력 (문자 지움)
+// 				putchar('\b'); // 커서를 다시 하나 뒤로 이동
+// 				idx--; // 인덱스 줄임
+// 				userSearchTerm[idx] = '\0'; // 문자열 끝 처리
+// 			}
+// 			continue; // 다음 루프로 이동
+// 		}
+
+// 		putchar(ch); // 입력받은 문자 출력
+		
+// 		userSearchTerm[idx++] = ch; // 문자를 검색어에 추가
+// 		userSearchTerm[idx] = '\0'; // 문자열 끝 처리
+
+// 		write(sock, userSearchTerm, strlen(userSearchTerm)); // 서버에 검색어 전송
+// 		// 서버로부터 검색 결과를 받아옴
+// 		char result[BUF_SIZE * MAX_LINES];
+// 		read(sock, result, sizeof(result));
+
+// 		// 화면을 지우고 새로운 검색 결과를 출력
+// 		system("clear");
+// 		printf("Search Word: %s\n", userSearchTerm);
+// 		printf("-----------------------------\n");
+// 		printWithHighlight(result, userSearchTerm);
+
+// 	}
+// 	return NULL;
+// }
+
+void * send_msg(void * arg)
 {
-    int sock=*((int*)arg);
-    char result[MAX_LINES * (BUF_SIZE + 20)];
-    int str_len = read(sock, result, sizeof(result) - 1);
-    if(str_len == -1) 
-        return (void*)-1;
-    result[str_len] = 0;
-    printf("%s", result);
+    int sock = *((int *)arg);
+    
+    int idx = 0;
+
+    printf("Search Word: "); // 처음 검색어 입력 프롬프트 출력
+
+    while (1)
+    {
+        int ch = getch(); // 문자 하나를 받아옴
+		 // 뮤텍스 잠그기
+        pthread_mutex_lock(&lock);
+
+        if (ch == '\n' || ch == EOF)
+        {
+            write(sock, userSearchTerm, strlen(userSearchTerm)); // 서버에 검색어 전송
+        }
+
+        if (ch == 127 || ch == 8) // 백스페이스 키 처리
+        {
+            if (idx > 0) // 이미 입력된 문자가 있을 때만
+            {
+                printf("\b \b"); // 백스페이스 처리
+                idx--; // 인덱스 줄임
+                userSearchTerm[idx] = '\0'; // 문자열 끝 처리
+            }
+        }
+        else
+        {
+            putchar(ch); // 입력받은 문자 출력
+            userSearchTerm[idx++] = ch; // 문자를 검색어에 추가
+            userSearchTerm[idx] = '\0'; // 문자열 끝 처리
+        }
+
+        write(sock, userSearchTerm, strlen(userSearchTerm)); // 서버에 검색어 전송
+
+		// 뮤텍스 해제
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
+}
+
+void * recv_msg(void * arg)
+{
+    int sock = *((int *)arg);
+    char result[BUF_SIZE * MAX_LINES] = {0}; // 초기화
+
+    while (1)
+    {
+        int bytes_read = read(sock, result, BUF_SIZE * MAX_LINES - 1); // 서버로부터 검색 결과를 받아옴
+        result[bytes_read] = '\0'; // 문자열의 끝을 수동으로 처리
+		printf("Received result from server: %s\n", result);
+		  // 뮤텍스 잠그기
+        pthread_mutex_lock(&lock);
+
+        // 화면을 지우고 새로운 검색 결과를 출력
+        system("clear");
+        printf("Search Word: %s\n", userSearchTerm);
+		 // 뮤텍스 해제
+        pthread_mutex_unlock(&lock);
+        printf("-----------------------------\n");
+        printWithHighlight(result, userSearchTerm);
+    }
+    return NULL;
+}
+
+
+int getch()
+{
+	int c = 0;
+	struct termios oldattr, newattr;
+
+	tcgetattr(STDIN_FILENO, &oldattr);
+	newattr = oldattr;
+	newattr.c_lflag &= ~(ICANON | ECHO);
+	newattr.c_cc[VMIN] = 1;
+	newattr.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+	c = getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+
+	return c;
 }
 
 	
@@ -129,6 +238,27 @@ void searchPacket(const char *searchTerm)
     }
     if (!found)
     {
-        printf("NOT FOUND: %s\n", searchTerm);
+        system("clear");
+		printf("NOT FOUND: %s\n", searchTerm);
+
+    }
+}
+void printWithHighlight(const char *text, const char *searchTerm) {
+    const char *p = text;
+    while (*p) {
+        const char *match = strstr(p, searchTerm);
+        if (match) {
+            while (p < match) {
+                putchar(*p++);
+            }
+            printf("\033[1;31m");  // 시작 ANSI 코드 (빨간색으로 강조)
+            while (*p && p < match + strlen(searchTerm)) {
+                putchar(*p++);
+            }
+            printf("\033[0m");  // 끝 ANSI 코드 (색상 초기화)
+        } else {
+            printf("%s", p);
+            break;
+        }
     }
 }
